fa3a6e3f55b2bcaada043530ab08b6da34133a18
PHANDERA <48357053+pat-phandera@users.noreply.github.com>
2019-04-26 18:55:37 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,50 @@
+const projename= document.getElementById("contactName");
+
+const projedescri= document.getElementById("contactNum");
+
+
+
+const button=document.getElementById("button");
+
+const IsOutup=document.getElementById("IsOutup");
+
+
+
+button.onclick=function (){
+
+    const myList=contactName.value;
+    const myval=contactNum.value;
+    
+
+    
+
+    console.log(myList);
+    console.log(myval);
+    
+
+    if(myList&&myval){
+
+        localStorage.setItem(myList,myval);
+    
+        location.reload();
+    }
+    
+
+    
+};
+IsOutup1.innerHTML +=`<h2>${" CONTACT NO.    "}<br>`;
+IsOutup.innerHTML +=`<h2>${"CONTACT-NAME   "}<br>`;
+ numbers.innerHTML+=` <h2>${"PHONE-NUMBER"}<br>`;
+ ContDiv.innerHTML+="TOTAL CONTACTS :"+localStorage.length;
+for(let i=0; i<localStorage.length; i++){
+    const myList=localStorage.key(i);
+    const myval=localStorage.getItem(myList);
+
+
+    IsOutup1.innerHTML +=`<h3>${i+1}<br>`;
+    IsOutup.innerHTML +=`<h3>${myList}<br>`;
+    numbers.innerHTML+=`<h3>${myval}<br>`;
+}
+
+
+
